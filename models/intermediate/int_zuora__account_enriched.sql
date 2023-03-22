16cf81f935aa47b9b7335d039b0c97ba580d7442
Avinash Kunnath <avinash.kunnath@fivetran.com>
2023-03-21 17:43:15 -0700

    Finalize first version of account models

--- models/intermediate/int_zuora__account_enriched.sql
+++ models/intermediate/int_zuora__account_enriched.sql
@@ -1,4 +1,10 @@
-with billing_history as (
+with account as (
+
+    select * 
+    from {{ var('account') }} 
+),
+
+billing_history as (
 
     select * 
     from {{ ref('zuora__billing_history') }}
@@ -16,14 +22,37 @@ invoice_item as (
     from {{ var('invoice_item')}}
 ),
 
+account_details as (
+
+    select 
+        account_id,
+        created_date,
+        name,
+        account_number,
+        credit_balance, 
+        mrr,
+        status,
+        auto_pay, 
+        safe_divide({{ dbt.datediff("account.created_date", dbt.current_timestamp_backcompat(), "day") }}, 12) as account_active_months,
+        case when {{ dbt.datediff("account.created_date", dbt.current_timestamp_backcompat(), "day") }} <= 30
+            then true else false end as is_new_customer
+    from account
+),
+
 account_totals as (
 
     select 
         account_id,
-        sum(tax_amount) as total_taxes,
         sum(refund_amount) as total_refunds,
         sum(discount_charges) as total_discounts,
-        sum(invoice_amount_home_currency) as total_invoice_balance_home_currency,
+        sum(tax_amount) as total_taxes,
+        sum(invoice_amount) as total_invoice_amount,
+        sum(invoice_amount_home_currency) as total_invoice_amount_home_currency,
+        sum(payment_amount) as total_amount_paid,
+        sum(balance) as total_amount_not_paid,
+        sum(case when cast({{ dbt.date_trunc('day', dbt.current_timestamp_backcompat()) }} as date) > due_date
+                and invoice_amount != payment_amount 
+                then balance else 0 end) as total_amount_past_due,
         max(payment_date) as most_recent_payment_date,
         max(credit_balance_adjustment_date) as most_recent_credit_balance_adjustment_date 
     from billing_history
@@ -43,6 +72,14 @@ account_invoice_data as (
     {{ dbt_utils.group_by(1) }}
 ),
 
+account_payment_data as (
+
+    select account_id,
+        sum(amount) as account_amount_paid
+    from {{ var('payment') }} 
+    {{ dbt_utils.group_by(1) }}
+),
+
 account_subscription_data as (
 
     select 
@@ -57,24 +94,44 @@ account_cumulatives as (
     
 select 
     account_totals.account_id,
+    account_details.created_date,
+    account_details.name,
+    account_details.account_number,
+    account_details.credit_balance, 
+    account_details.mrr,
+    account_details.status,
+    account_details.auto_pay,
+    account_details.account_active_months,
+    account_details.is_new_customer,
     account_totals.total_taxes,
     account_totals.total_refunds,
     account_totals.total_discounts,
-    account_totals.total_invoice_balance_home_currency,
+    account_totals.total_invoice_amount,
+    account_totals.total_invoice_amount_home_currency,
+    account_totals.total_amount_paid,
+    account_totals.total_amount_not_paid,
+    account_totals.total_amount_past_due,
     account_totals.most_recent_payment_date,
     account_totals.most_recent_credit_balance_adjustment_date, 
     account_invoice_data.first_charge_date,
     account_invoice_data.most_recent_charge_date,
     account_invoice_data.invoice_item_count,
     account_invoice_data.invoice_count,
+    safe_divide(account_totals.total_invoice_amount, account_invoice_data.invoice_count) as average_invoice_value,
+    safe_divide(account_invoice_data.invoice_item_count, account_invoice_data.invoice_count) as units_per_invoice,
     account_subscription_data.subscription_count,
     account_subscription_data.active_subscription_count,
-    case when account_subscription_data.active_subscription_count = 0 then false else true end as is_current_subscriber
-from account_totals
+    case when account_subscription_data.active_subscription_count = 0 then false else true end as is_currently_subscribed,
+    account_payment_data.account_amount_paid
+from account_details
+    left join account_totals
+        on account_details.account_id = account_totals.account_id
     left join account_invoice_data
         on account_totals.account_id = account_invoice_data.account_id
     left join account_subscription_data
         on account_totals.account_id = account_subscription_data.account_id
+    left join account_payment_data
+        on account_totals.account_id = account_payment_data.account_id
 )
 
 select * 
--- models/zuora__account_overview.sql
+++ models/zuora__account_overview.sql
@@ -1,35 +1,24 @@
-with account as (
+with account_enriched as (
 
     select * 
-    from {{ var('account') }} 
+    from {{ ref('int_zuora__account_enriched') }} 
 ),
 
 contact as (
 
     select * 
     from {{ var('contact') }} 
-),
-
-account_enriched as (
-
-    select * 
-    from {{ ref('int_zuora__account_enriched') }} 
 )
 
 select 
-    account.account_id, 
-    account.created_date as account_created_at,
-    case when {{ dbt.datediff("account.created_date", dbt.current_timestamp(), "day") }} <= 30
-        then true else false end as is_new_account,
-    account.name as account_name,
-    account.account_number,
-    account.balance as account_balance,
-    account.total_invoice_balance,
-    account.credit_balance,
-    account.unapplied_balance,
-    account.mrr as account_mrr,
-    account.status as account_status,
-    account.auto_pay as is_auto_pay,
+    account_enriched.account_id, 
+    account_enriched.created_date as created_at,
+    account_enriched.name as account_name,
+    account_enriched.account_number,
+    account_enriched.credit_balance,
+    account_enriched.mrr as account_mrr,
+    account_enriched.status as account_status,
+    account_enriched.auto_pay as is_auto_pay,
     contact.country as account_country,
     contact.city as account_city,
     contact.work_email as account_email,
@@ -37,18 +26,38 @@ select
     contact.last_name as account_last_name,
     contact.postal_code as account_postal_code,
     contact.state as account_state,
-    account_enriched.first_charge_date,
+    account_enriched.first_charge_date as first_charge_processed_at,
+    account_enriched.is_currently_subscribed,
+    account_enriched.is_new_customer,
     account_enriched.invoice_item_count,
     account_enriched.invoice_count,
-    account_enriched.subscription_count,
-    account_enriched.active_subscription_count,
-    account_enriched.is_current_subscriber
+    account_enriched.active_subscription_count as number_of_active_subscriptions,
+    account_enriched.subscription_count as number_of_subscriptions,
+    account_enriched.total_invoice_amount as total_amount_of_charges,
+    account_enriched.total_invoice_amount_home_currency as total_amount_of_charges_home_currency,
+    account_enriched.total_taxes as total_amount_of_taxes,
+    account_enriched.total_discounts as total_amount_of_discounts,
+    account_enriched.total_amount_paid,
+    account_enriched.total_amount_not_paid,
+    account_enriched.total_amount_past_due,
+    account_enriched.total_refunds,
+    account_enriched.average_invoice_value,
+    account_enriched.units_per_invoice,
+    safe_divide(account_enriched.active_subscription_count, account_enriched.account_active_months) as active_subscriptions_per_month,
+    safe_divide(account_enriched.subscription_count, account_enriched.account_active_months) as subscriptions_per_month,
+    safe_divide(account_enriched.total_invoice_amount, account_enriched.account_active_months) as charges_per_month,
+    safe_divide(account_enriched.total_invoice_amount_home_currency, account_enriched.account_active_months) as home_currency_charges_per_month,
+    safe_divide(account_enriched.total_taxes, account_enriched.account_active_months) as taxes_per_month, 
+    safe_divide(account_enriched.total_discounts, account_enriched.account_active_months) as discounts_per_month,
+    safe_divide(account_enriched.total_amount_paid, account_enriched.account_active_months) as amount_paid_per_month,
+    safe_divide(account_enriched.total_amount_not_paid, account_enriched.account_active_months) as amount_not_paid_per_month,
+    safe_divide(account_enriched.total_amount_past_due, account_enriched.account_active_months) as amount_past_due_per_month,
+    safe_divide(account_enriched.total_refunds, account_enriched.account_active_months) as refunds_per_month,
+    safe_divide(account_enriched.average_invoice_value, account_enriched.account_active_months) as invoice_value_per_month, 
+    safe_divide(account_enriched.units_per_invoice, account_enriched.account_active_months) as units_per_invoice_per_month
     
-    {{ fivetran_utils.persist_pass_through_columns('zuora_account_pass_through_columns', identifier='account') }},
+    {{ fivetran_utils.persist_pass_through_columns('zuora_account_pass_through_columns', identifier='account') }}
 
-
-from account
+from account_enriched
     left join contact 
-        on account.account_id = contact.account_id
-    left join account_enriched
-        on account.account_id = account_enriched.account_id 
+        on account_enriched.account_id = contact.account_id
